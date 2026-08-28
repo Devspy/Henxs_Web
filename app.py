0e3ec95c9f7a4b24b0ca98a24ec3b7abd225fc41
Dev <dev.mandal@oditeksolutions.com>
2026-08-28 15:12:39 +0530

    change logo and make ip base from localhost

--- app.py
+++ app.py
@@ -94,4 +94,8 @@ def contact():
     return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.getenv('PORT', '5000')),
+        debug=True
+    )
